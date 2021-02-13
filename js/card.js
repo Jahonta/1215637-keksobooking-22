c8006c8d08344c55586a3f175751577e59463794
Lydia Zakharova <jahontis@gmail.com>
2021-02-13 11:22:35 +0300

    рендерит объявление

---
+++ js/card.js
@@ -0,0 +1,43 @@
+import {ads} from './data.js';
+
+const mapElement = document.querySelector('.map__canvas');
+const cardTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.popup');
+
+const typesMap = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+};
+
+const renderFeatures = (features) => {
+  return features.map((feature) => {
+    return `<li class="popup__feature popup__feature--${feature}"></li>`
+  }).join('');
+};
+
+const renderPhotos = (photos) => {
+  return photos.map((photo) => {
+    return `<img src="${photo}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`
+  }).join('');
+};
+
+const renderAd = ({author, offer}) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__avatar').src = author.avatar;
+  cardElement.querySelector('.popup__title').textContent = offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  cardElement.querySelector('.popup__type').textContent = typesMap[offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  cardElement.querySelector('.popup__features').innerHTML = renderFeatures(offer.features);
+  cardElement.querySelector('.popup__description').textContent = offer.description;
+  cardElement.querySelector('.popup__photos').innerHTML = renderPhotos(offer.photos);
+
+  mapElement.append(cardElement);
+};
+
+renderAd(ads[0]);
