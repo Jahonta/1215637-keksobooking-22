388a920234d9f58fca4a8d5b37df1bd8571a2d0a
Lydia Zakharova <jahontis@gmail.com>
2021-02-18 20:32:13 +0300

    добавляет валидацию заголовка

--- js/form.js
+++ js/form.js
@@ -22,6 +22,16 @@ const mapFeatureFields = mapFilterElement.querySelectorAll('.map__checkbox');
 const adFormFields = [typeField, priceField, timeInField, timeOutField, avatarField, titleField, addressField, capacityField, descriptionField, imagesField, roomNumberField, ...featureFields];
 const mapFilterFields = [housingTypeField, housingPriceField, housingRoomsField, housingGuestsField, mapFeatureFields];
 
+const FieldLimit = {
+  MIN_TITLE_LENGTH: 30,
+  MAX_TITLE_LENGTH: 100,
+  MAX_PRICE: 1000000,
+  MIN_BUNGALOW_PRICE: 0,
+  MIN_FLAT_PRICE: 1000,
+  MIN_HOUSE_PRICE: 5000,
+  MIN_PALACE_PRICE: 10000,
+}
+
 const typesToPrices = {
   flat: 1000,
   bungalow: 0,
@@ -29,18 +39,12 @@ const typesToPrices = {
   palace: 10000,
 };
 
-typeField.addEventListener('change', () => {
-  priceField.placeholder = typesToPrices[typeField.value];
-  priceField.min = typesToPrices[typeField.value];
-});
-
-timeInField.addEventListener('change', () => {
-  timeOutField.value = timeInField.value;
-});
-
-timeOutField.addEventListener('change', () => {
-  timeInField.value = timeOutField.value;
-});
+const roomsToGuests = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0'],
+};
 
 const turnFormOff = (form, fields) => {
   form.classList.add('ad-form--disabled');
@@ -69,5 +73,34 @@ const setActiveState = () => {
 const setAddress = ({lat, lng}) => {
   addressField.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
 };
+
+const validateTitle = () => {
+  const valueLength = titleField.value.length;
+  let message = '';
+
+  if (valueLength < FieldLimit.MIN_TITLE_LENGTH) {
+    message = 'Ещё ' + (FieldLimit.MIN_TITLE_LENGTH - valueLength) + ' симв.';
+  }
+  if (valueLength > FieldLimit.MAX_NAME_LENGTH) {
+    message = 'Удалите лишние ' + (valueLength - FieldLimit.MAX_NAME_LENGTH) + ' симв.';
+  }
+
+  titleField.setCustomValidity(message);
+};
+
+typeField.addEventListener('change', () => {
+  priceField.placeholder = typesToPrices[typeField.value];
+  priceField.min = typesToPrices[typeField.value];
+});
+
+timeInField.addEventListener('change', () => {
+  timeOutField.value = timeInField.value;
+});
+
+timeOutField.addEventListener('change', () => {
+  timeInField.value = timeOutField.value;
+});
+
+titleField.addEventListener('invalid', validateTitle);
 
 export {setInactiveState, setActiveState, setAddress};
